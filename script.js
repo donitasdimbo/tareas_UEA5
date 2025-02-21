93bb8ac2864bb8ee123e59d0b201a1df55609079
donitasdimbo <2233034021@correo.ler.uam.mx>
2025-02-21 08:21:31 -0600

    tareas

---
+++ script.js
@@ -0,0 +1,4 @@
+
+console.log("invocaron al mago");
+window.alert("se adentran a territorio enemigo");
+document.write("todos somos java y tu tambien lo seras");
---
+++ variables/script.js
@@ -0,0 +1,17 @@
+// variables 
+
+let var1 = "esencia";
+let var2 = 5 ;
+//document.write(let_Var1);
+document.getElementById('demo').innerHTML="una magia es : "+ var1 ; 
+
+
+let weas1 = "ojo de sapo";
+let weas2 = "ruda";
+let weas3 = "babosas"
+
+let weasp1 = 150; 
+let weasp2 = 20 ;
+let weasp3 = 60 ;
+
+document.getElementById('demo2').innerHTML="ocupas : "+ weas1 + "  y este cuesta: "+ weasp1; 

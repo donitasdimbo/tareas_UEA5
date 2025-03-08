b44f8ac5ceb379df94be5064eae26964cd2828ef
donitasdimbo <2233034021@correo.ler.uam.mx>
2025-03-07 22:52:31 -0600

    reflexion

---
+++ Reflexión_lenguajesProgramacion/script.js
@@ -0,0 +1,15 @@
+
+let p1= "Los lenguajes de programación vistos en clases (HTML, CSS, JS) son los más utilizados para la programación web de diversos sitios, al ser los más utilizados esto permite que cada vez se busque añadir más propiedades para realizar nuevas cosas. Estos lenguajes son los que nos permiten realizar obras artísticas o una simple página con información y una estética neutra, considero que son lenguajes que se pueden dominar y hacer grandes cosas con los conocimientos básicos pero si se perfeccionan se pueden hacer cosas más laboriosas y asombrosas. ";
+let p2= "Considerando las pocas cosas que se aún no podría hacer obras muy complejas pero si me interesa la idea de las páginas en las que se desarrolla novelas visuales ya sea por medio de scrolling, me gustaría retarme y hacer una página de ese estilo. La forma en la que se complementa los tres lenguajes de programación mencionados desde un inicio, ya que HTML si se trabaja por si solo se pueden hacer buenas páginas, pero le faltan muchos recursos para poder dar más diversidad de diseño pero estos recursos CSS los tiene así que las paginas tienen mas creatividad u originalidad por las formas en las que se pueden tratar las imágenes y el texto. ";
+let p3= "Para la creación de programación creativa siento que estos lenguajes son muy buenos pero seria mas facil que ya un lenguaje posea lo que estos tres tiene combinados, aunque sería más complejo saber todo lo necesario para una programación adecuada.";
+
+document.getElementById('parr1').innerHTML= p1;
+
+document.getElementById('parr2').innerHTML= p2;
+
+document.getElementById('parr3').innerHTML= p3;
+
+console.log(p1.length);   /* para saber por medio de la consola cuantos caracteres tiene ese parrafo*/ 
+console.log(p2.length);
+console.log(p3.length);
+
--- TAREA2/script.js
+++
@@ -1,7 +0,0 @@
-
-let p1= "escribiremos aqui";
-let p2= "igual aqui";
-
-document.getElementById('parr1').innerHTML= p1;
-
-document.getElementById('parr2').innerHTML= p2;
